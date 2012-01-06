6b12bb60c6ab1d72fb5239df7b3a9f62aee79945
Josh Bohde <josh.bohde@gmail.com>
2012-01-06 00:15:23 -0600

    Back to flymake+pyflakes, with new keybindings.

--- .emacs.d/config/config-python.el
+++ .emacs.d/config/config-python.el
@@ -6,7 +6,25 @@
 (autoload 'python-pep8 "python-pep8")
 (require 'python-pylint)
 (autoload 'pylint "python-pylint")
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
+
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (define-key py-mode-map "\M-p" 'flymake-goto-prev-error)
+            (define-key py-mode-map "\M-n" 'flymake-goto-next-error)))
+
+(when (load "flymake" t)
+  (require 'flymake-cursor)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+               'flymake-create-temp-inplace))
+       (local-file (file-relative-name
+            temp-file
+            (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+           '("\\.py\\'" flymake-pyflakes-init)))
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
 
 (provide 'config-python)

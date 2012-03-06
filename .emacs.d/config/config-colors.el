54c9e5988bb881125bdcb6bf171213756818bb8d
Josh Bohde <josh@joshbohde.com>
2012-03-05 23:03:20 -0600

    Use eighties color scheme.

--- .emacs.d/config/config-colors.el
+++ .emacs.d/config/config-colors.el
@@ -3,6 +3,6 @@
 (setq color-theme-is-global t)
 
 (package-require 'color-theme-sanityinc-tomorrow)
-(color-theme-sanityinc-tomorrow-bright)
+(color-theme-sanityinc-tomorrow-eighties)
 
 (provide 'config-colors)
